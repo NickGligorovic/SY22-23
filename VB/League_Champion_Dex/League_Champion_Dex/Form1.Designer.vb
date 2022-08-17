70c1284a42c9e23ca3b34f8df4f23e67767faef3
Nick Gligorovic <nbgli875@learner.dvusd.org>
2022-08-17 14:06:00 -0700

    Wednesday Update 8/17/2022

---
+++ VB/League_Champion_Dex/League_Champion_Dex/Form1.Designer.vb
@@ -0,0 +1,280 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TahmKenchButton = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.NameLabel = New System.Windows.Forms.Label()
+        Me.LaneLabel = New System.Windows.Forms.Label()
+        Me.ClassLabel = New System.Windows.Forms.Label()
+        Me.RaceLabel = New System.Windows.Forms.Label()
+        Me.PoppyButton = New System.Windows.Forms.Button()
+        Me.EzrealButton = New System.Windows.Forms.Button()
+        Me.LuxButton = New System.Windows.Forms.Button()
+        Me.AattroxButton = New System.Windows.Forms.Button()
+        Me.AattroxPictureBox = New System.Windows.Forms.PictureBox()
+        Me.LuxPictureBox = New System.Windows.Forms.PictureBox()
+        Me.EzrealPictureBox = New System.Windows.Forms.PictureBox()
+        Me.PoppyPictureBox = New System.Windows.Forms.PictureBox()
+        Me.TahmPictureBox = New System.Windows.Forms.PictureBox()
+        Me.displayPictureBox = New System.Windows.Forms.PictureBox()
+        CType(Me.AattroxPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.LuxPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.EzrealPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PoppyPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TahmPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.displayPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TahmKenchButton
+        '
+        Me.TahmKenchButton.Location = New System.Drawing.Point(43, 269)
+        Me.TahmKenchButton.Name = "TahmKenchButton"
+        Me.TahmKenchButton.Size = New System.Drawing.Size(75, 33)
+        Me.TahmKenchButton.TabIndex = 0
+        Me.TahmKenchButton.Text = "Tahm Kench"
+        Me.TahmKenchButton.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(380, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(47, 16)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Name:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(383, 46)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(40, 16)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Lane:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(383, 82)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(44, 16)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Class:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(383, 116)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(43, 16)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Race:"
+        '
+        'NameLabel
+        '
+        Me.NameLabel.AutoSize = True
+        Me.NameLabel.Location = New System.Drawing.Point(434, 13)
+        Me.NameLabel.Name = "NameLabel"
+        Me.NameLabel.Size = New System.Drawing.Size(0, 16)
+        Me.NameLabel.TabIndex = 7
+        '
+        'LaneLabel
+        '
+        Me.LaneLabel.AutoSize = True
+        Me.LaneLabel.Location = New System.Drawing.Point(437, 45)
+        Me.LaneLabel.Name = "LaneLabel"
+        Me.LaneLabel.Size = New System.Drawing.Size(0, 16)
+        Me.LaneLabel.TabIndex = 8
+        '
+        'ClassLabel
+        '
+        Me.ClassLabel.AutoSize = True
+        Me.ClassLabel.Location = New System.Drawing.Point(437, 82)
+        Me.ClassLabel.Name = "ClassLabel"
+        Me.ClassLabel.Size = New System.Drawing.Size(0, 16)
+        Me.ClassLabel.TabIndex = 9
+        '
+        'RaceLabel
+        '
+        Me.RaceLabel.AutoSize = True
+        Me.RaceLabel.Location = New System.Drawing.Point(440, 116)
+        Me.RaceLabel.Name = "RaceLabel"
+        Me.RaceLabel.Size = New System.Drawing.Size(0, 16)
+        Me.RaceLabel.TabIndex = 10
+        '
+        'PoppyButton
+        '
+        Me.PoppyButton.Location = New System.Drawing.Point(204, 269)
+        Me.PoppyButton.Name = "PoppyButton"
+        Me.PoppyButton.Size = New System.Drawing.Size(75, 32)
+        Me.PoppyButton.TabIndex = 11
+        Me.PoppyButton.Text = "Poppy"
+        Me.PoppyButton.UseVisualStyleBackColor = True
+        '
+        'EzrealButton
+        '
+        Me.EzrealButton.Location = New System.Drawing.Point(365, 269)
+        Me.EzrealButton.Name = "EzrealButton"
+        Me.EzrealButton.Size = New System.Drawing.Size(75, 32)
+        Me.EzrealButton.TabIndex = 13
+        Me.EzrealButton.Text = "Ezreal"
+        Me.EzrealButton.UseVisualStyleBackColor = True
+        '
+        'LuxButton
+        '
+        Me.LuxButton.Location = New System.Drawing.Point(513, 269)
+        Me.LuxButton.Name = "LuxButton"
+        Me.LuxButton.Size = New System.Drawing.Size(75, 32)
+        Me.LuxButton.TabIndex = 15
+        Me.LuxButton.Text = "Lux"
+        Me.LuxButton.UseVisualStyleBackColor = True
+        '
+        'AattroxButton
+        '
+        Me.AattroxButton.Location = New System.Drawing.Point(665, 269)
+        Me.AattroxButton.Name = "AattroxButton"
+        Me.AattroxButton.Size = New System.Drawing.Size(75, 31)
+        Me.AattroxButton.TabIndex = 17
+        Me.AattroxButton.Text = "Aattrox"
+        Me.AattroxButton.UseVisualStyleBackColor = True
+        '
+        'AattroxPictureBox
+        '
+        Me.AattroxPictureBox.Image = Global.League_Champion_Dex.My.Resources.Resources.aatrox
+        Me.AattroxPictureBox.Location = New System.Drawing.Point(636, 308)
+        Me.AattroxPictureBox.Name = "AattroxPictureBox"
+        Me.AattroxPictureBox.Size = New System.Drawing.Size(140, 128)
+        Me.AattroxPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.AattroxPictureBox.TabIndex = 18
+        Me.AattroxPictureBox.TabStop = False
+        '
+        'LuxPictureBox
+        '
+        Me.LuxPictureBox.Image = Global.League_Champion_Dex.My.Resources.Resources.lux
+        Me.LuxPictureBox.Location = New System.Drawing.Point(494, 309)
+        Me.LuxPictureBox.Name = "LuxPictureBox"
+        Me.LuxPictureBox.Size = New System.Drawing.Size(126, 129)
+        Me.LuxPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.LuxPictureBox.TabIndex = 16
+        Me.LuxPictureBox.TabStop = False
+        '
+        'EzrealPictureBox
+        '
+        Me.EzrealPictureBox.Image = Global.League_Champion_Dex.My.Resources.Resources.ezreal
+        Me.EzrealPictureBox.Location = New System.Drawing.Point(337, 308)
+        Me.EzrealPictureBox.Name = "EzrealPictureBox"
+        Me.EzrealPictureBox.Size = New System.Drawing.Size(139, 129)
+        Me.EzrealPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.EzrealPictureBox.TabIndex = 14
+        Me.EzrealPictureBox.TabStop = False
+        '
+        'PoppyPictureBox
+        '
+        Me.PoppyPictureBox.Image = Global.League_Champion_Dex.My.Resources.Resources.thumb_1920_663587
+        Me.PoppyPictureBox.Location = New System.Drawing.Point(178, 307)
+        Me.PoppyPictureBox.Name = "PoppyPictureBox"
+        Me.PoppyPictureBox.Size = New System.Drawing.Size(138, 130)
+        Me.PoppyPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PoppyPictureBox.TabIndex = 12
+        Me.PoppyPictureBox.TabStop = False
+        '
+        'TahmPictureBox
+        '
+        Me.TahmPictureBox.Image = Global.League_Champion_Dex.My.Resources.Resources.download
+        Me.TahmPictureBox.Location = New System.Drawing.Point(12, 308)
+        Me.TahmPictureBox.Name = "TahmPictureBox"
+        Me.TahmPictureBox.Size = New System.Drawing.Size(148, 130)
+        Me.TahmPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.TahmPictureBox.TabIndex = 2
+        Me.TahmPictureBox.TabStop = False
+        '
+        'displayPictureBox
+        '
+        Me.displayPictureBox.Location = New System.Drawing.Point(1, -1)
+        Me.displayPictureBox.Name = "displayPictureBox"
+        Me.displayPictureBox.Size = New System.Drawing.Size(372, 237)
+        Me.displayPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.displayPictureBox.TabIndex = 1
+        Me.displayPictureBox.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.AattroxPictureBox)
+        Me.Controls.Add(Me.AattroxButton)
+        Me.Controls.Add(Me.LuxPictureBox)
+        Me.Controls.Add(Me.LuxButton)
+        Me.Controls.Add(Me.EzrealPictureBox)
+        Me.Controls.Add(Me.EzrealButton)
+        Me.Controls.Add(Me.PoppyPictureBox)
+        Me.Controls.Add(Me.PoppyButton)
+        Me.Controls.Add(Me.RaceLabel)
+        Me.Controls.Add(Me.ClassLabel)
+        Me.Controls.Add(Me.LaneLabel)
+        Me.Controls.Add(Me.NameLabel)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TahmPictureBox)
+        Me.Controls.Add(Me.displayPictureBox)
+        Me.Controls.Add(Me.TahmKenchButton)
+        Me.Name = "Form1"
+        Me.Text = "League_Champions_Dex"
+        CType(Me.AattroxPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.LuxPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.EzrealPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PoppyPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TahmPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.displayPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TahmKenchButton As Button
+    Friend WithEvents displayPictureBox As PictureBox
+    Friend WithEvents TahmPictureBox As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents NameLabel As Label
+    Friend WithEvents LaneLabel As Label
+    Friend WithEvents ClassLabel As Label
+    Friend WithEvents RaceLabel As Label
+    Friend WithEvents PoppyButton As Button
+    Friend WithEvents PoppyPictureBox As PictureBox
+    Friend WithEvents EzrealButton As Button
+    Friend WithEvents EzrealPictureBox As PictureBox
+    Friend WithEvents LuxButton As Button
+    Friend WithEvents LuxPictureBox As PictureBox
+    Friend WithEvents AattroxButton As Button
+    Friend WithEvents AattroxPictureBox As PictureBox
+End Class
---
+++ VB/League_Champion_Dex/League_Champion_Dex/Form1.vb
@@ -0,0 +1,42 @@
+﻿Public Class Form1
+    Private Sub TahmKenchButton_Click(sender As Object, e As EventArgs) Handles TahmKenchButton.Click
+        displayPictureBox.Image = TahmPictureBox.Image
+        NameLabel.Text = "Tahm Kentch"
+        LaneLabel.Text = "Top"
+        ClassLabel.Text = "Tank"
+        RaceLabel.Text = "Demon"
+    End Sub
+
+    Private Sub PoppyButton_Click(sender As Object, e As EventArgs) Handles PoppyButton.Click
+        displayPictureBox.Image = PoppyPictureBox.Image
+        NameLabel.Text = "Poppy"
+        LaneLabel.Text = "Top or Jungle"
+        ClassLabel.Text = "Bruiser/Tank"
+        RaceLabel.Text = "Yordle"
+    End Sub
+
+    Private Sub EzrealButton_Click(sender As Object, e As EventArgs) Handles EzrealButton.Click
+        displayPictureBox.Image = EzrealPictureBox.Image
+        NameLabel.Text = "Ezreal"
+        LaneLabel.Text = "Bottom"
+        ClassLabel.Text = "Marksman"
+        RaceLabel.Text = "Human"
+
+    End Sub
+
+    Private Sub LuxButton_Click(sender As Object, e As EventArgs) Handles LuxButton.Click
+        displayPictureBox.Image = LuxPictureBox.Image
+        NameLabel.Text = "Lux"
+        LaneLabel.Text = "Mid"
+        ClassLabel.Text = "Mage"
+        RaceLabel.Text = "Human"
+    End Sub
+
+    Private Sub AattroxButton_Click(sender As Object, e As EventArgs) Handles AattroxButton.Click
+        displayPictureBox.Image = AattroxPictureBox.Image
+        NameLabel.Text = "Aattrox"
+        LaneLabel.Text = "Top"
+        ClassLabel.Text = "Bruiser"
+        RaceLabel.Text = "Demon"
+    End Sub
+End Class
